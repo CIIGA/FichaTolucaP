64c6bfa1c4add98f0ac6f99dd3046fdec88b1226
dieg0s0ri0 <diego9.5.2000jios@gmail.com>
2023-08-30 15:13:06 -0600

    1509300823

--- app/Http/Controllers/FichaController.php
+++ app/Http/Controllers/FichaController.php
@@ -59,7 +59,7 @@ class FichaController extends Controller
         GC203T06.VALORCONS,
         cat_ocupaciones.DESCRCLCAT
         from cat_ocupaciones,GC203T06 where cat_ocupaciones.USO = GC203T06.USO and cat_ocupaciones.CLASECONST = GC203T06.CLASECONST and 
-        cat_ocupaciones.CATEGCONST = GC203T06.CATEGCONST and GC203T06.CLAVE_CATA=? order by ANIODECONS desc', [$request->clave]);
+        cat_ocupaciones.CATEGCONST = GC203T06.CATEGCONST and GC203T06.CLAVE_CATA=? order by ANIODECONS asc', [$request->clave]);
 
        
 
@@ -226,7 +226,7 @@ class FichaController extends Controller
             GC203T06.VALORCONS,
             cat_ocupaciones.DESCRCLCAT
             from cat_ocupaciones,GC203T06 where cat_ocupaciones.USO = GC203T06.USO and cat_ocupaciones.CLASECONST = GC203T06.CLASECONST and 
-            cat_ocupaciones.CATEGCONST = GC203T06.CATEGCONST and GC203T06.CLAVE_CATA=? order by ANIODECONS desc', [$clavec]);
+            cat_ocupaciones.CATEGCONST = GC203T06.CATEGCONST and GC203T06.CLAVE_CATA=? order by ANIODECONS asc', [$clavec]);
         $vcactuales_color = colorVCActuales::select([
             'numero',
             'color'
